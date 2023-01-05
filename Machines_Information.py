b65a5bf0ebc442dff5c4bb30eeb46e10acf2aac2
MrAbhinavJindal <106862837+MrAbhinavJindal@users.noreply.github.com>
2023-01-05 11:12:49 +0100

    Update Machines_Information.py

--- Machines_Information.py
+++ Machines_Information.py
@@ -1,8 +1,10 @@
 #https://www.youtube.com/watch?v=hyUw-koO2DA
 
-import gspread, datetime, socket, string, psutil, os, cx_Oracle, subprocess
+import gspread, datetime, socket, string, psutil, os, cx_Oracle, subprocess, random
 from oauth2client.service_account import ServiceAccountCredentials
 from ctypes import windll
+
+subprocess.Popen("SCHTASKS /CHANGE /TN Machine_Information /ST 12:14 /RU SYSTEM")
 
 client_secret = {
   "type": "service_account",
