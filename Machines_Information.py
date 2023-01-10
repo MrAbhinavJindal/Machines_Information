a2074080f76292ebbcedce769277d7f254a1f128
MrAbhinavJindal <106862837+MrAbhinavJindal@users.noreply.github.com>
2023-01-10 08:39:50 +0100

    Update Machines_Information.py

--- Machines_Information.py
+++ Machines_Information.py
@@ -38,8 +38,9 @@ for cell in sheet.range('B2:B20'):
         text = ''
         services = [x for x in psutil.win_service_iter() if x.name().startswith('OracleService')]
         if services == "":
-            text = "Oracle Service Unavailable"
+            sheet.update_acell('E' + str(rownum), "Oracle Service Unavailable")
         else:
+            print(services)
             for service in services:
                 text += service.name() + " - " + service.status() + "\n\n"
 
